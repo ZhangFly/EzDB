ea241b639c1bf0b09a4e508b8a7d572bdd6b6394
YFei <z.outstanding@outlook.com>
2016-06-04 18:29:07 +0800

    添加了部分注释,导出最新jar包

---
+++ src/zfly/yfei/db/YFeiSQLExecutor.java
@@ -0,0 +1,70 @@
+package zfly.yfei.db;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+
+/**
+ * 简单SQL语句执行器，内部维持有简单连接池
+ *
+ * @author YFei
+ */
+public class YFeiSqlExecutor {
+
+    private static Logger log = Logger.getLogger(YFeiSqlExecutor.class);
+    private SimpleConnectionPool pool;
+
+    /**
+     * 构造函数,仅包内可访问
+     *
+     * @param size 内部连接池大小
+     * @param url  数据库连接地址
+     * @param usr  数据库用户名
+     * @param pwd  数据库密码
+     * @throws SQLException
+     */
+    YFeiSqlExecutor(final int size, final String url, final String usr, final String pwd) throws SQLException {
+        pool = new SimpleConnectionPool(size, url, usr, pwd);
+    }
+
+    /**
+     * 执行SQL语句
+     *
+     * @param sql SQL语句
+     * @throws SQLException
+     */
+    public void doExecute(final String sql) throws SQLException {
+        doExecute(sql, null);
+    }
+
+    /**
+     * @param sql     SQL语句
+     * @param handler 执行成功后的服务方法
+     * @throws SQLException
+     */
+    public void doExecute(final String sql, final YFeiDBSqlExecutorHandler handler) throws SQLException {
+
+        final Connection conn = pool.request();
+
+        try {
+            final Statement stat = conn.createStatement();
+            if (StringUtils.containsIgnoreCase(sql, "select")) {
+                final ResultSet res = stat.executeQuery(sql);
+                if (handler != null) {
+                    handler.onDone(res);
+                }
+            } else {
+                stat.execute(sql);
+            }
+            stat.close();
+            pool.release(conn);
+        } catch (SQLException e) {
+            pool.release(conn);
+            throw e;
+        }
+    }
+}
